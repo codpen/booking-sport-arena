df75c065105f5bad5496d22171034087287a5894
codpen <davidpeng913@gmail.com>
2022-04-21 21:37:40 +0700

    Add navbar to Home

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,18 +1,11 @@
-import logo from '../assets/logo.svg';
-import { OutlineButton, SolidButton } from '../components/Buttons';
+import Navbar from '../components/Navbar';
 import '../styles/App.css';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<div className='flex'>
-					<SolidButton text='Login' link='/login' />
-
-					<OutlineButton text='Register' link='/Register' />
-				</div>
-			</header>
+			<Navbar />
+			<header className='App-header'></header>
 		</div>
 	);
 }
