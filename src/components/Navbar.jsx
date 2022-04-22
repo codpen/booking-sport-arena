e7618915c0a478e918cac6865413db95ba508bb7
codpen <davidpeng913@gmail.com>
2022-04-22 13:21:26 +0700

    Implement Alert to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { OutlineButton, SolidButton } from './Buttons';
 import { useNavigate } from 'react-router';
+import { loginAlert } from '../functions/Alert';
 
 export default function Navbar() {
 	const navigate = useNavigate();
@@ -8,8 +9,7 @@ export default function Navbar() {
 		if (localStorage.getItem('token')) {
 			navigate('/user');
 		} else {
-			alert('Please Login');
-			navigate('/login');
+			loginAlert(navigate);
 		}
 	};
 
@@ -17,16 +17,12 @@ export default function Navbar() {
 		<nav className='shadow-md mb-2'>
 			<div className='flex justify-between mx-32'>
 				<h4 className='my-auto font-bold text-2xl text-teal-500'>
-					Hobiku
+					<a href='/'>Hobiku</a>
 				</h4>
 
 				<div className=''>
 					<SolidButton text='Login' link='/login' onClick={isLogin} />
-					<OutlineButton
-						text='Register'
-						link='/Register'
-						onClick={isLogin}
-					/>
+					<OutlineButton text='Register' link='/Register' />
 				</div>
 			</div>
 		</nav>
