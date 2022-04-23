b45b12463884bb0394bdb1039acf2b23b99571cd
codpen <davidpeng913@gmail.com>
2022-04-23 09:14:31 +0700

    auth login

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export function SolidButton({ text, link, onClick }) {
-	return (
-		<button
-			className='m-2 bg-teal-500 text-white px-5 py-2 rounded-md font-bold'
-			onClick={onClick}>
-			<Link to={link}>{text}</Link>
-		</button>
-	);
+  return (
+    <button
+      className="m-2 bg-teal-500 text-white px-5 py-2 rounded-md font-bold"
+      onClick={onClick}
+    >
+      <Link to={link}>{text}</Link>
+    </button>
+  );
 }
 
 export function OutlineButton({ text, link, onClick }) {
-	return (
-		<button
-			className='m-2 outline-teal-500 text-teal-500 outline outline-2 px-5 py-2 rounded-md font-bold'
-			onClick={onClick}>
-			<Link to={link}>{text}</Link>
-		</button>
-	);
+  return (
+    <button
+      className="m-2 outline-teal-500 text-teal-500 outline outline-2 px-5 py-2 rounded-md font-bold"
+      onClick={onClick}
+    >
+      <Link to={link}>{text}</Link>
+    </button>
+  );
 }
