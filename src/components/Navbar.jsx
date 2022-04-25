ed8c59e4d17d993624e546bc508c2bbd571da576
codpen <davidpeng913@gmail.com>
2022-04-25 15:39:57 +0700

    fix config API delete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from './Buttons';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "./Buttons";
 
 export default function Navbar() {
-	const navigate = useNavigate();
-	return (
-		<nav className='shadow-md mb-2'>
-			<div className='flex justify-between mx-32'>
-				<h4 className='my-auto font-bold text-2xl text-teal-500'>
-					<a href='/'>Hobiku</a>
-				</h4>
+  const navigate = useNavigate();
+  return (
+    <nav className="shadow-md mb-2">
+      <div className="flex justify-between mx-32">
+        <h4 className="my-auto font-bold text-2xl text-teal-500 text">
+          <a href="/">Hobiku</a>
+        </h4>
 
-				<div className=''>
-					<Button
-						variant='solid'
-						className='my-3 mx-1'
-						onClick={() => navigate('/login')}>
-						Login
-					</Button>
-					<Button
-						variant='outline'
-						className='my-3 mx-1'
-						onClick={() => navigate('/register')}>
-						Register
-					</Button>
-				</div>
-			</div>
-		</nav>
-	);
+        <div className="">
+          <Button
+            variant="solid"
+            className="my-3 mx-1"
+            onClick={() => navigate("/login")}
+          >
+            Login
+          </Button>
+          <Button
+            variant="outline"
+            className="my-3 mx-1"
+            onClick={() => navigate("/register")}
+          >
+            Register
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
 }
