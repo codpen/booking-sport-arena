cbb2748493502cd86f83c6d778293881ec2a81e5
codpen <davidpeng913@gmail.com>
2022-04-26 23:40:43 +0700

    update UI
    - add background image
    - add search input & button

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,27 @@
+import Button from '../components/Buttons';
 import { ListCategory } from '../components/Category';
+import { InputText } from '../components/InputText';
 import Layout from '../components/Layout';
 import PaginationRounded from '../components/Pagination';
 import { ListVenue } from '../components/Venue';
+import '../styles/App.css';
 
 function App() {
 	return (
 		<>
 			<Layout>
+				<div className="banner">
+					<div className="cover">
+						<div className="h-full grid gap-4 content-center">
+							<div className="mx-auto flex flex-row gap-2">
+								<InputText type="text" placeholder="Search" />
+								<Button variant="solid" className="" size="lg">
+									Search
+								</Button>
+							</div>
+						</div>
+					</div>
+				</div>
 				<ListCategory />
 				<ListVenue />
 				<PaginationRounded />
