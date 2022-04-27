c059035a08f4d39286fd14953c86a4c4ce437282
codpen <davidpeng913@gmail.com>
2022-04-27 13:37:16 +0700

    fix responsiveness category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,8 +7,10 @@ export const Category = ({ categories }) => {
 	return (
 		<>
 			{categories.map((category) => (
-				<div key={category.id} className="grid grid-cols-4 gap-4">
-					<div className="rounded-3xl border-2 outline-gray-500 text-center p-4">
+				<div
+					key={category.id}
+					className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+					<div className="rounded-3xl border-2 outline-gray-500 text-center p-2 md:p-4 lg:p-4 xl:p-4 2xl:p-4">
 						{/* <i className={`text-6xl p-3 ${category.icon_name}`} /> */}
 						<i className={`text-6xl p-3 fa-solid fa-basketball`} />
 						<h5 className="text-lg">{category.name}</h5>
@@ -39,7 +41,7 @@ export function ListCategory() {
 
 	return (
 		<>
-			<h4 className="uppercase text-2xl my-8 text-teal-500 font-bold text-center">
+			<h4 className="uppercase text-xl md:text-xl lg:text-2xl xl:text-2xl 2xl:text-3xl  my-8 text-teal-500 font-bold text-center">
 				Category
 			</h4>
 			<Category categories={category} />
