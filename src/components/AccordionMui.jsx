49c36a027d13971df6d9093cea7f2386e4100e5a
codpen <davidpeng913@gmail.com>
2022-04-27 15:32:06 +0700

    add accordion

--- src/components/AccordionMui.jsx
+++ src/components/AccordionMui.jsx
@@ -1,9 +1,12 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Avatar } from "@mui/material";
+import { Document } from "react-pdf";
+import { Page } from "react-pdf";
 
 export default function AccordionMui() {
   const [expanded, setExpanded] = React.useState(false);
@@ -23,82 +26,53 @@ export default function AccordionMui() {
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>
-            General settings
-          </Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            I am an accordion
-          </Typography>
+          <div className="w-full">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <Avatar
+                  sx={{ width: 56, height: 56 }}
+                  alt="Remy Sharp"
+                  src="/static/images/avatar/1.jpg"
+                />
+                <div className="ml-4">
+                  <strong>full name naem name</strong>
+                  <p>username</p>
+                </div>
+              </div>
+
+              <div>
+                <strong>mail@mail.com</strong>
+                <p>+91-1234567890</p>
+              </div>
+
+              <div>
+                <strong>may 26, 2019</strong>
+                <p>6.30pm</p>
+              </div>
+
+              <div>
+                <button className="w-auto bg-yellow-400 px-6 py-1 rounded-full mr-20 text-white">
+                  Pending
+                </button>
+              </div>
+            </div>
+          </div>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        expanded={expanded === "panel2"}
-        onChange={handleChange("panel2")}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2bh-content"
-          id="panel2bh-header"
-        >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>Users</Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            You are currently not an owner
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Donec placerat, lectus sed mattis semper, neque lectus feugiat
-            lectus, varius pulvinar diam eros in elit. Pellentesque convallis
-            laoreet laoreet.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        expanded={expanded === "panel3"}
-        onChange={handleChange("panel3")}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3bh-content"
-          id="panel3bh-header"
-        >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>
-            Advanced settings
-          </Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            Filtering has been entirely disabled for whole web server
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
-            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        expanded={expanded === "panel4"}
-        onChange={handleChange("panel4")}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel4bh-content"
-          id="panel4bh-header"
-        >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>
-            Personal data
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
-            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
+            <div className="w-full">
+              <div className="flex items-center justify-between">
+                <div>nama file.pdf</div>
+                <div>
+                  <button className="w-auto bg-emerald-600 px-6 py-1 rounded-full mr-5 text-white">
+                    approve
+                  </button>
+                  <button className="w-auto bg-red-600 px-6 py-1 rounded-full mr-20 text-white">
+                    reject
+                  </button>
+                </div>
+              </div>
+            </div>
           </Typography>
         </AccordionDetails>
       </Accordion>
