d33a7f8215ed87b8123caf75977a1fe28b489f9f
codpen <davidpeng913@gmail.com>
2022-04-27 23:59:46 +0700

    add component card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -23,3 +23,16 @@ export function CardLoading(item) {
 		</>
 	);
 }
+
+export function IconCard(item) {
+	return (
+		<>
+			<div key={item.id} className="">
+				<div className="rounded-3xl border-2 outline-gray-500 text-center p-2 md:p-4 lg:p-4 xl:p-4 2xl:p-4">
+					<i className={`text-4xl p-2 ${item.icon}`} />
+					<h5 className="font-bold uppercase">{item.name}</h5>
+				</div>
+			</div>
+		</>
+	);
+}
