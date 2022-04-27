6ebb4005375aea887920c43793af2258f0f72c49
codpen <davidpeng913@gmail.com>
2022-04-27 11:32:01 +0700

    add pdf

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -7,7 +7,16 @@ export default function AdminDashboard() {
     <div className="w-screen h-screen flex">
       <Sidebar />
       {/* <Accordion /> */}
-      <AccordionMui />
+      <div className="w-full border-2 rounded-lg mb-10 mx-8 mt-20 py-3">
+        <p className="text-lg font-semibold pt-2 pb-6 ml-7"> owner Request</p>
+        <div className="flex text-slate-500 mb-3 justify-around ">
+          <p>User</p>
+          <p>Contact</p>
+          <p>Date</p>
+          <p>Status</p>
+        </div>
+        <AccordionMui />
+      </div>
     </div>
   );
 }
