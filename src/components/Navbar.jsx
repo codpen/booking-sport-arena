80bcd55761577e8bb68a0d2ba3478efc35bedb92
codpen <davidpeng913@gmail.com>
2022-04-28 13:35:27 +0700

    add user dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,14 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import Button from "./Buttons";
-import user from '../assets/user.png';
+import UserDropdown from "./Dropdown";
 
 export default function Navbar() {
-	const navigate = useNavigate();
-	const isLogin = localStorage.getItem('user-info');
 	return (
 		<nav className="shadow-md mb-2">
 			<div className="flex justify-between mx-8 md:mx-10 lg:mx-28 xl:mx-32">
 				<h4 className="my-auto font-bold text-2xl text-teal-500 text">
 					<a href="/">Hobiku</a>
 				</h4>
-
-				<Button
-					variant="outline"
-					className="my-3"
-					onClick={() => {
-						if (isLogin) {
-							navigate('/user');
-						} else {
-							navigate('/login');
-						}
-					}}>
-					<img src={user} alt="" height={20} width={20} />
-				</Button>
+				<UserDropdown />
 			</div>
 		</nav>
 	);
