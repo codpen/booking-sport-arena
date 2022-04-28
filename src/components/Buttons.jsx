093497ab52c4807b974368554006c35f3b6939c6
codpen <davidpeng913@gmail.com>
2022-04-28 13:45:42 +0700

    update option to add id in button

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -7,27 +7,26 @@ export default function Button({
 	children,
 	onClick,
 	type,
+	id,
 }) {
-	const addClassName = className ? `${className}` : '';
+	const addClassName = className ? `${className}` : "";
 
 	const variants = {
-		solid: 'bg-teal-500 text-white',
-		outline: 'border-teal-500 text-teal-500 border',
-		warning: ' bg-amber-500 text-white',
-		danger: 'bg-red-500 text-white',
+		solid: "bg-teal-500 text-white",
+		outline: "border-teal-500 text-teal-500 border",
+		warning: " bg-amber-500 text-white",
+		danger: "bg-red-500 text-white",
 	};
 
 	const pickedVariant = variants[variant];
 
 	return (
 		<button
+			id={id}
 			className={`px-5 py-2 rounded-md font-bold ${pickedVariant} ${addClassName}`}
 			onClick={onClick}
 			type={type}>
 			{children}
 		</button>
 	);
-  
-  
-  
 }
