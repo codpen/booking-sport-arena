a878a47995dd32f5ddbd28073e5ccb8877031c94
codpen <davidpeng913@gmail.com>
2022-05-04 11:21:31 +0700

    setting overflow-vh100

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,9 +4,9 @@ import Navbar from '../components/Navbar';
 export default function Layout({ children }) {
 	return (
 		<>
-			<div className="bg-gray-50 overflow-auto">
+			<div className="bg-gray-50 overflow-auto h-screen">
 				<Navbar />
-				<div className="h-screen px-8 md:px-10 lg:px-24 xl:px-32 2xl:px-40">
+				<div className="px-8 md:px-10 lg:px-24 xl:px-32 2xl:px-40">
 					{children}
 				</div>
 			</div>
