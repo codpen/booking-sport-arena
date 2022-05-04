b21c2c6300499db77ca52a22c6818a9b20e82d06
codpen <davidpeng913@gmail.com>
2022-05-04 22:29:42 +0700

    add layout style for owner

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { ResponsiveDrawer } from "./Drawer";
 
 export default function Layout({ children }) {
 	return (
@@ -9,6 +10,16 @@ export default function Layout({ children }) {
 				<div className="px-8 md:px-10 lg:px-24 xl:px-32 2xl:px-40">
 					{children}
 				</div>
+			</div>
+		</>
+	);
+}
+
+export function LayoutOwner({ children }) {
+	return (
+		<>
+			<div className="bg-gray-50 overflow-auto h-screen">
+				<ResponsiveDrawer>{children}</ResponsiveDrawer>
 			</div>
 		</>
 	);
