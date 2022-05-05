e150b4259850f734eec915e5cf4e38b700748335
codpen <davidpeng913@gmail.com>
2022-05-06 06:22:15 +0700

    create dropdown list based on condition ifLogin

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -3,6 +3,7 @@ import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import user from "../assets/user.png";
 import { useNavigate } from "react-router-dom";
+import { statusLogin } from "../services/Users";
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(" ");
@@ -11,9 +12,7 @@ function classNames(...classes) {
 export default function UserDropdown() {
 	const navigate = useNavigate();
 	const getToken = localStorage.getItem("user-info");
-	const isLogin = getToken
-		? Object.values(JSON.parse(getToken)).toString()
-		: getToken;
+	const isLogin = getToken ? statusLogin() : getToken;
 	return (
 		<>
 			<Menu as="div" className="relative inline-block text-left">
@@ -32,53 +31,45 @@ export default function UserDropdown() {
 					leave="transition ease-in duration-75"
 					leaveFrom="transform opacity-100 scale-100"
 					leaveTo="transform opacity-0 scale-95">
-					<Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-						<div className="py-1">
-							<Menu.Item>
-								{({ active }) => (
-									// eslint-disable-next-line
-									<a
-										href="#"
-										className={classNames(
-											active
-												? "bg-gray-100 text-gray-900"
-												: "text-gray-700",
-											"block px-4 py-2 text-sm"
-										)}
-										onClick={() => {
-											if (isLogin) {
+					<Menu.Items className="origin-top-right absolute right-0 mt-2 w-32 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+						{isLogin ? (
+							<div className="py-1">
+								<Menu.Item>
+									{({ active }) => (
+										// eslint-disable-next-line
+										<a
+											href="#"
+											className={classNames(
+												active
+													? "bg-gray-100 text-gray-900"
+													: "text-gray-700",
+												"block px-4 py-2 text-sm"
+											)}
+											onClick={() => {
 												navigate("/user");
-											} else {
-												navigate("/login");
-											}
-										}}>
-										Account settings
-									</a>
-								)}
-							</Menu.Item>
-							<Menu.Item>
-								{({ active }) => (
-									// eslint-disable-next-line
-									<a
-										href="#"
-										className={classNames(
-											active
-												? "bg-gray-100 text-gray-900"
-												: "text-gray-700",
-											"block px-4 py-2 text-sm"
-										)}
-										onClick={() => {
-											if (isLogin) {
+											}}>
+											Profile
+										</a>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										// eslint-disable-next-line
+										<a
+											href="#"
+											className={classNames(
+												active
+													? "bg-gray-100 text-gray-900"
+													: "text-gray-700",
+												"block px-4 py-2 text-sm"
+											)}
+											onClick={() => {
 												navigate("/user/history");
-											} else {
-												navigate("/login");
-											}
-										}}>
-										History
-									</a>
-								)}
-							</Menu.Item>
-							<form>
+											}}>
+											History
+										</a>
+									)}
+								</Menu.Item>
 								<Menu.Item>
 									{({ active }) => (
 										<button
@@ -99,8 +90,43 @@ export default function UserDropdown() {
 										</button>
 									)}
 								</Menu.Item>
-							</form>
-						</div>
+							</div>
+						) : (
+							<div className="py-1">
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											href="#"
+											className={classNames(
+												active
+													? "bg-gray-100 text-gray-900"
+													: "text-gray-700",
+												"block w-full text-left px-4 py-2 text-sm"
+											)}
+											onClick={() => navigate("/login")}>
+											Sign in
+										</button>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											href="#"
+											className={classNames(
+												active
+													? "bg-gray-100 text-gray-900"
+													: "text-gray-700",
+												"block w-full text-left px-4 py-2 text-sm"
+											)}
+											onClick={() =>
+												navigate("/register")
+											}>
+											Register
+										</button>
+									)}
+								</Menu.Item>
+							</div>
+						)}
 					</Menu.Items>
 				</Transition>
 			</Menu>
