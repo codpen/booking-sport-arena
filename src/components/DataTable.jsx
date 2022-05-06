b4a598e79cdb8047fadd05c66afd1754ce32ae24
codpen <davidpeng913@gmail.com>
2022-05-06 14:59:00 +0700

    change Toolbar to customToolbar

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,7 +1,14 @@
 import * as React from "react";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import {
+	DataGrid,
+	GridToolbarContainer,
+	GridToolbarFilterButton,
+} from "@mui/x-data-grid";
+
+import PropTypes from "prop-types";
 
 export default function DataTable({ rows, columns }) {
+	const [filterButtonEl, setFilterButtonEl] = React.useState(null);
 	return (
 		<div style={{ width: "100%", height: 450 }}>
 			<DataGrid
@@ -10,10 +17,28 @@ export default function DataTable({ rows, columns }) {
 				pageSize={10 ? 10 : rows.length}
 				rowsPerPageOptions={[10]}
 				components={{
-					Toolbar: GridToolbar,
+					Toolbar: CustomToolbar,
+				}}
+				componentsProps={{
+					panel: {
+						anchorEl: filterButtonEl,
+					},
+					toolbar: {
+						setFilterButtonEl,
+					},
 				}}
 				disableSelectionOnClick
 			/>
 		</div>
 	);
 }
+
+const CustomToolbar = ({ setFilterButtonEl }) => (
+	<GridToolbarContainer>
+		<GridToolbarFilterButton ref={setFilterButtonEl} />
+	</GridToolbarContainer>
+);
+CustomToolbar.propTypes = {
+	setFilterButtonEl: PropTypes.func.isRequired,
+};
+ 
