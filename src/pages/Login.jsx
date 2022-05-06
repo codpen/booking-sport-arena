4572d152996668aac619030024dc0ff22f297384
codpen <davidpeng913@gmail.com>
2022-05-06 15:44:08 +0700

    fix input email and number

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,70 +14,68 @@ export default function Login() {
   document.title = "Login";
 
   async function signIn() {
-		let Auth = await loginService({
-			email: email,
-			password: password,
-		});
-		if (Auth.code === 200) {
-			localStorage.setItem("user-info", JSON.stringify(Auth.data));
-			swal(Auth.message, "", "success");
-			navigate("/");
-		} else if (Auth.code === 400) {
-			swal("Login failed", Auth.message, "error");
-		}
+    let Auth = await loginService({
+      email: email,
+      password: password,
+    });
+    if (Auth.code === 200) {
+      localStorage.setItem("user-info", JSON.stringify(Auth.data));
+      swal(Auth.message, "", "success");
+      navigate("/");
+    } else if (Auth.code === 400) {
+      swal("Login failed", Auth.message, "error");
+    }
   }
 
   return (
-		<div>
-			<Navbar />
-			<div
-				className="flex justify-center pt-28 md:pt-24 lg:pt-32 h-screen bg-no-repeat bg-cover px-10 md:px-40 lg:px-64"
-				style={{
-					backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://images.unsplash.com/photo-1574629810360-7efbbe195018?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1293&q=80")`,
-				}}>
-				<div className="h-fit w-auto bg-white rounded-md py-10 px-4 md:px-24 lg:px-40 uppercase">
-					<h1 className="text-3xl font-bold text-center text-teal-500 mb-7">
-						Login
-					</h1>
-					<p className=" mb-2 font-semibold text-center">Email</p>
-					<InputText
-						onChange={(e) => setEmail(e.target.value)}
-						id="input-email"
-						type="text"
-						placeholder="mail@mail.com"
-					/>
-					<p className=" mb-2 mt-6 font-semibold text-center">
-						Password
-					</p>
-					<InputText
-						onChange={(e) => setPassword(e.target.value)}
-						id="input-password"
-						placeholder="Password"
-						type="password"
-						className="mb-4"
-					/>
-					<Button
-						id="login-button"
-						type="submit"
-						variant="solid"
-						className="mt-10 w-full uppercase"
-						onClick={() => {
-							signIn();
-						}}>
-						login
-					</Button>
-					<p className="text-center mt-3 capitalize">
-						doesn't have an account?
-						<a
-							id="navigate-register"
-							className="text-cyan-600"
-							href="Register">
-							{" "}
-							Register
-						</a>
-					</p>
-				</div>
-			</div>
-		</div>
+    <div>
+      <Navbar />
+      <div
+        className="flex justify-center pt-28 md:pt-24 lg:pt-32 h-screen bg-no-repeat bg-cover px-10 md:px-40 lg:px-64"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://images.unsplash.com/photo-1574629810360-7efbbe195018?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1293&q=80")`,
+        }}
+      >
+        <div className="h-fit w-auto bg-white rounded-md py-10 px-4 md:px-24 lg:px-40 uppercase">
+          <h1 className="text-3xl font-bold text-center text-teal-500 mb-7">
+            Login
+          </h1>
+          <p className=" mb-2 font-semibold text-center">Email</p>
+          <InputText
+            onChange={(e) => setEmail(e.target.value)}
+            id="input-email"
+            type="email"
+            placeholder="mail@mail.com"
+            className="invalid:text-red-500"
+          />
+          <p className=" mb-2 mt-6 font-semibold text-center">Password</p>
+          <InputText
+            onChange={(e) => setPassword(e.target.value)}
+            id="input-password"
+            placeholder="Password"
+            type="password"
+            className="mb-4"
+          />
+          <Button
+            id="login-button"
+            type="submit"
+            variant="solid"
+            className="mt-10 w-full uppercase"
+            onClick={() => {
+              signIn();
+            }}
+          >
+            login
+          </Button>
+          <p className="text-center mt-3 capitalize">
+            doesn't have an account?
+            <a id="navigate-register" className="text-cyan-600" href="Register">
+              {" "}
+              Register
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
